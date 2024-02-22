c82e7a40ef40af659261d1c7c6be610bc6a2bf7c
shpakvlad <vladyslav.shpakov@gmail.com>
2024-02-22 08:36:21 +0200

    add body styles

--- src/pages/MessagesForKey/MessagesForKey.tsx
+++ src/pages/MessagesForKey/MessagesForKey.tsx
@@ -1,15 +1,25 @@
 import styles from './MessagesForKey.module.scss';
 import {Header_2} from '@components';
+import Header_3 from '@/components/Header_3/Header_3.tsx';
 
 
 const MessagesForKey = () => {
     return (
         <div className={styles.MessagesForKey}>
-        
-            <Header_2 title={'СООБЩЕНИЕ ДЛЯ КЛЮЧА PC'}/>
 
-            <textarea cols={66} rows={10}/>
-        
+            <div className={styles.message}>
+
+                <Header_2 title={'СООБЩЕНИЕ ДЛЯ КЛЮЧА PC'}/>
+
+                <textarea cols={66} rows={10}/>
+            </div>
+
+            <div className={styles.currentMessage}>
+
+                <Header_3 title={'ТЕКУЩЕЕ СООБЩЕНИЕ'}/>
+
+            </div>
+
         </div>
     );
 };
