d881cd36c8cf9d552df12b652536f96f3a06b0ac
Andrei <116939459+Bababum95@users.noreply.github.com>
2024-02-23 08:25:15 +0700

    update link active

--- src/pages/Statistics/Statistics.tsx
+++ src/pages/Statistics/Statistics.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import 'chart.js/auto';
+import { Chart } from 'react-chartjs-2';
 
 const Statistics = () => {
     return (
-        <div>Statistics</div>
+        <Chart type='line' data={{
+            labels: ['Jun', 'Jul', 'Aug'],
+            datasets: [
+                {
+                    // id: 1,
+                    label: '',
+                    data: [5, 6, 7],
+                },
+                {
+                    // id: 2,
+                    label: '',
+                    data: [3, 2, 1],
+                },
+            ],
+        }} />
     );
 };
 
