949eb69f4a38893b9bb77b9b2297e9e971ff1c2c
Andrei <116939459+Bababum95@users.noreply.github.com>
2024-02-24 19:50:22 +0700

    chart component statistics page hedar/aside positions

--- src/pages/Statistics/Statistics.tsx
+++ src/pages/Statistics/Statistics.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -9,10 +9,11 @@ import {
     Tooltip,
     Legend,
 } from 'chart.js';
-import { Line } from 'react-chartjs-2';
 import faker from 'faker';
 
+import Chart from './Chart';
 import styles from './Statistics.module.scss';
+import classNames from 'classnames';
 
 ChartJS.register(
     CategoryScale,
@@ -57,38 +58,76 @@ const data = {
 };
 
 
-
 const Statistics = () => {
     return (
-        <Line
-            className={styles.chart}
-            width={424}
-            height={235}
-            options={{
-                responsive: false,
-                aspectRatio: 424 / 235,
-                plugins: {
-                    legend: {
-                        display: false,
-                    },
-                },
-                scales: {
-                    y: {
-                        grid: {
-                            color: '#8E8E93',
-                            drawTicks: false,
-                        },
-                    },
-                    x: {
-                        grid: {
-                            color: 'transparent',
-                            tickColor: '#8E8E93',
-                        },
-                    },
-                },
-            }}
-            data={data}
-        />
+        <div>
+            <ul className={styles.list}>
+                <li className={styles.item}>
+                    <span className={styles.label}>Зарегистрированные пользователи</span>
+                    <span className={styles.value}>58,000</span>
+                </li>
+                <li className={styles.item}>
+                    <span className={styles.label}>Онлайн игроков</span>
+                    <span className={styles.value}>1332</span>
+                </li>
+                <li className={styles.item}>
+                    <span className={styles.label}>Средний чек пользователя</span>
+                    <span className={styles.value}>500 ТМТ</span>
+                </li>
+                <li className={styles.item}>
+                    <span className={styles.label}>Всего промокодов</span>
+                    <span className={styles.value}>58,000</span>
+                </li>
+            </ul>
+            <div className={styles.buttons}>
+                <button className={classNames(styles.button, styles.active)}>
+                    Сегодня
+                </button>
+                <button className={styles.button}>Вчера</button>
+                <button className={styles.button}>Неделя</button>
+                <button className={styles.button}>Месяц</button>
+                <button className={styles.button}>Квартал</button>
+                <button className={styles.button}>Год</button>
+            </div>
+            <div className={styles.charts}>
+                <Chart
+                    data={data}
+                    title='Онлайн игроки по ключу PC'
+                    label='Онлайн'
+                    value={315}
+                />
+                <Chart
+                    data={data}
+                    title='Онлайн игроки по ключу Android'
+                    label='Онлайн'
+                    value={315}
+                />
+                <Chart
+                    data={data}
+                    title='Онлайн игроки по ключу Steam'
+                    label='Онлайн'
+                    value={315}
+                />
+                <Chart
+                    data={data}
+                    title='Выручка от тарифов'
+                    label='Выручка'
+                    value={315}
+                />
+                <Chart
+                    data={data}
+                    title='Выручка от донатов'
+                    label='Выручка'
+                    value={315}
+                />
+                <Chart
+                    data={data}
+                    title='Активаций промокодов'
+                    label='Активаций промокодов'
+                    value={315}
+                />
+            </div>
+        </div>
     );
 
 };
