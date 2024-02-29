e5aa0d0852c580c77e11feb4ff13047d14eccac6
shpakvlad <shpakov.vladyslav@gmail.com>
2024-02-29 19:41:06 +0200

    major fix

--- src/pages/Donut/Donut.tsx
+++ src/pages/Donut/Donut.tsx
@@ -9,37 +9,37 @@ const Donut = () => {
 
             <Heading title='Донат' size={3}/>
 
-            <div className={styles.donutContent}>
-                <label className={styles.label}>Название</label>
-                <input className={styles.input} type='text'/>
+            <div className={styles.сontent}>
+                <label className={styles.label} htmlFor='name'>Название</label>
+                <input className={styles.input} name='name' type='text'/>
 
-                <label className={styles.label}  htmlFor='game'>Игра</label>
-                <input className={styles.input} type='text'/>
+                <label className={styles.label} htmlFor='game'>Игра</label>
+                <input className={styles.input} name='game' type='text'/>
 
-                <label className={styles.label}  htmlFor='timeLabel'>Отметка</label>
-                <input className={styles.input} type='text'/>
+                <label className={styles.label} htmlFor='timeLabel'>Отметка</label>
+                <input className={styles.input} name='timeLabel' type='text'/>
 
-                <label className={styles.label}  htmlFor='user'>Пользователь</label>
-                <input className={styles.input} type='text'/>
+                <label className={styles.label} htmlFor='user'>Пользователь</label>
+                <input className={styles.input} name='user' type='text'/>
 
-                <label className={styles.label}  htmlFor='userID'>Пользователя</label>
-                <input className={styles.input} type='text'/>
+                <label className={styles.label} htmlFor='userID'>Пользователя</label>
+                <input className={styles.input} name='userID' type='text'/>
 
-                <label className={styles.label}  htmlFor='userNick'>Никнейм в игре</label>
-                <input className={styles.input} type='text'/>
+                <label className={styles.label} htmlFor='userNick'>Никнейм в игре</label>
+                <input className={styles.input} name='userNick' type='text'/>
 
-                <label className={styles.label}  htmlFor='totalMoney'>Сумма</label>
-                <input className={styles.input} type='text'/>
+                <label className={styles.label} htmlFor='totalMoney'>Сумма</label>
+                <input className={styles.input} name='totalMoney' type='text'/>
 
-                <label className={styles.label}  htmlFor='status'>Изменить статус</label>
-                <div className={styles.buttons}>
-                    <button className={classNames(styles.button, styles.unsuccessful)}>Неудачно</button>
+                <label className={styles.label}>Изменить статус</label>
+                <div className={styles.buttons} >
+                    <button className={styles.button}>Неудачно</button>
                     <button className={classNames(styles.button, styles.pending)}>В обработке</button>
-                    <button className={classNames(styles.button, styles.paid)}>Оплачен</button>
+                    <button className={styles.button}>Оплачен</button>
                 </div>
             </div>
 
-            <div className={styles.footerButtons}>
+            <div className={styles.footer}>
                 <button className={styles.button}>Отмена</button>
                 <button className={classNames(styles.button, styles.save)}>Сохранить изменения</button>
             </div>
--- src/pages/EditGame/EditGame.tsx
+++ src/pages/EditGame/EditGame.tsx
@@ -7,29 +7,31 @@ const EditGame = () => {
         <div className={styles.editGame}>
             <Heading title='Редактировать игру' size={3}/>
 
-            <div className={styles.content}>
-                <label className={styles.label} htmlFor='name'>Название</label>
-                <input className={styles.input} type='text' name='name'/>
+            <form action='#'>
+                <div className={styles.content}>
+                    <label className={styles.label} htmlFor='name'>Название</label>
+                    <input className={styles.input} type='text' name='name'/>
 
-                <label className={styles.label} htmlFor='link'>Ссылка</label>
-                <input className={styles.input} type='text' name='link'/>
+                    <label className={styles.label} htmlFor='link'>Ссылка</label>
+                    <input className={styles.input} type='text' name='link'/>
 
-                <label className={styles.label} htmlFor='category'>Категория</label>
-                <select className={styles.select} name='category' id='category'>
-                    <option value='nomoney'>Бесплатно</option>
-                    <option value='standartLib'>Стандартная библиотека</option>
-                    <option value='extendetLib'>Расширенная библиотека</option>
-                </select>
-            </div>
-
-            <div className={styles.buttonsPanel}>
-                <button className={classNames(styles.button, styles.deleteGameBtn)}>Удалить игру</button>
-
-                <div className={styles.additionalButtons}>
-                    <button className={classNames(styles.button, styles.cancelBtn)}>Отмена</button>
-                    <button className={classNames(styles.button, styles.saveGameBtn)}>Сохранить изменения</button>
+                    <label className={styles.label} htmlFor='category'>Категория</label>
+                    <select className={styles.select} name='category' id='category'>
+                        <option value='nomoney'>Бесплатно</option>
+                        <option value='standartLib'>Стандартная библиотека</option>
+                        <option value='extendetLib'>Расширенная библиотека</option>
+                    </select>
                 </div>
-            </div>
+
+                <div className={styles.footer}>
+                    <button className={styles.button}>Удалить игру</button>
+
+                    <div>
+                        <button className={styles.button}>Отмена</button>
+                        <button className={classNames(styles.button, styles.save)}>Сохранить изменения</button>
+                    </div>
+                </div>
+            </form>
         </div>
     );
 };
