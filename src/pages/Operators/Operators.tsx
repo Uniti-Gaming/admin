0db56169c673a887be04069d43917e2e73a3d0ab
shpakvlad <shpakov.vladyslav@gmail.com>
2024-05-09 19:41:15 +0300

    Added Operators page

---
+++ src/pages/Operators/Operators.tsx
@@ -0,0 +1,14 @@
+import styles from './Operators.module.scss';
+
+
+const Operators = () => {
+    return (
+        <div className={styles.Operators}>
+
+            <h1>Operators</h1>
+
+        </div>
+    )
+};
+
+export default Operators;
